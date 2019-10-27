2bac9b75de3eb86cd88d92dcee71783d233b4be1
18020967 <56230874+18020967@users.noreply.github.com>
2019-10-28 01:12:51 +0200

    Version 2.0

---
+++ POE-3D rts/Assets/Scripts/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void start()
+
+    {
+        PlayPause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void PlayPause()
+    {
+        if (Time.timeScale != 0.001f)
+        {
+            Time.timeScale = 0.001f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+    }
+}
